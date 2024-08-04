8d900bd1b4b6009b1ab443ea42cb5718d2e639be
GustavoHenriqueDEV <gubochniavelho@gmail.com>
2024-08-03 23:47:10 -0300

    Parte II - Tutorial 4

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import EventCard from '@/components/EventCard.vue'
+import EventCard from '@/components/EventCard.vue';
 import { ref } from 'vue'
 
 const events = ref([
@@ -39,16 +39,12 @@ const events = ref([
 ])
 </script>
 
-...
 <template>
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
-...
-
-
 
 <style scoped>
 .events {
